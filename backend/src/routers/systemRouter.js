d7176c5273ef0105e23712ed01a4b198e6ff1ce8
hoangthienofficial <hoangthienofficial05204@gmail.com>
2025-02-09 21:24:03 +0700

    Xóa folder Backend và client khỏi nhánh

--- backend/src/routers/systemRouter.js
+++
@@ -1,9 +0,0 @@
-
-   import express from 'express';
-   import { getSystemInfo } from '../controllers/systemController.js';
-
-   const routersystem = express.Router();
-
-   routersystem.get('/system-info', getSystemInfo);
-
-   export default routersystem;
--- client/src/service/banner/banner.service.tsx
+++
@@ -1,6 +0,0 @@
-import { axiosWithAuth } from "../axios.config";
-export const getAllBanner = () => {
-    return axiosWithAuth("/banner", {
-        method: "GET",
-    });
-};
