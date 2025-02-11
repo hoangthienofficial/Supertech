63c1f73c1e6261c2d35ea128496020f1b79c86ca
hoangthienofficial <hoangthienofficial05204@gmail.com>
2025-02-12 02:13:46 +0700

    update user21

--- backend/src/controllers/uploadImageUserController.js
+++ backend/src/controllers/uploadImageUserController.js
@@ -1,6 +1,6 @@
 import cloudinary from "../config/cloudinaryConfig.js";
 import multer from "multer";
-import ImageModel from "../models/User.js";
+import ImageModel from "../models/user.js";
 import path from "path";
 import { responseSend } from "../config/response.js";
 const storage = multer.memoryStorage();
