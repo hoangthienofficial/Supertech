db4fd64fcf753c8331922de526db8903c5ac4924
hoangthienofficial <hoangthienofficial05204@gmail.com>
2025-02-12 02:18:48 +0700

    update213

--- backend/src/index.js
+++ backend/src/index.js
@@ -48,7 +48,7 @@ const corsOptions = {
   ],
   credentials: true,
 };
-// cron 1 1 này chỉ chạy được trên server thôi, local tạm thời ẩn, tuyệt đối ko tắt để hạn chế tình trạng trùng lập cron giữa local
+// cron này chỉ chạy được trên server thôi, local tạm thời ẩn, tuyệt đối ko tắt để hạn chế tình trạng trùng lập cron giữa local
 
 cron.schedule("* * * * *", async () => {
   console.log("Bắt đầu chạy Check Lịch Sử Giao Dịch Api Từ Phía Ngân Hàng!!!");
