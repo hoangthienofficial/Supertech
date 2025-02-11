bdf4a17dc9ea2ffb28a6ad76c837b096768711a2
hoangthienofficial <hoangthienofficial05204@gmail.com>
2025-02-12 02:31:16 +0700

    update

--- backend/src/routers/transactionRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { checkTransactionStatus } from '../controllers/transactionController.js';
-
-const transactionsrouter = express.Router();
-
-transactionsrouter.get("/check-transactions", checkTransactionStatus);
-
-export default transactionsrouter;
